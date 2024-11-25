e2d7db7e381f8ef8d8c25c814e7e7126ba0709e0
Roberson Borges <robersonfborges@gmail.com>
2024-11-25 19:49:24 -0300

    finalizada main e criada footer

--- src/Pages/Inicio/inicio.jsx
+++ src/Pages/Inicio/inicio.jsx
@@ -1,15 +1,48 @@
 
 import S from "./inicio.module.scss";
+import Balanca from "../../assets/balanca.png";
+import Community from "../../assets/community.png";
+import Transform from "../../assets/transform.png";
+import Reading from "../../assets/reading.png";
 
-
-export default function Inicio(){
-    return (
-        <section>
-            <section className={S.post}>
-                <h2>
-                VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO
-                </h2>
-            </section>
+export default function Inicio() {
+  return (
+    <main>
+      <section className={S.post}>
+        <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
+      </section>
+      <section className={S.devoDoar}>
+        <div>
+          <h2> POR QUE DEVO DOAR ?</h2>
+        </div>
+        <section className={S.cards}>
+          <article>
+            <img src={Community} alt="" />
+            <p>
+              Oferece livros a quem não tem acesso, ajudando a reduzir a
+              exclusão social.
+            </p>
+          </article>
+          <article>
+            <img src={Reading} alt="" />
+            <p>Estimula o hábito da leitura e o aprendizado contínuo.</p>
+          </article>
+          <article>
+            <img src={Transform} alt="" />
+            <p>
+              Fornece conhecimento e inspiração, permitindo que indivíduos
+              transformem suas vidas.
+            </p>
+          </article>
+          <article>
+            <img src={Balanca} alt="" />
+            <p>
+              Garante que todos, independentemente de sua condição, tenham
+              oportunidades de aprendizado.
+            </p>
+          </article>
         </section>
-    )
+      </section>
+    </main>
+  );
 }
--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,9 +1,41 @@
-export default function Footer(){
-    return (
-        <footer>
-            <p>
-                estamos na FOOTER
-            </p>
-        </footer>
-    )
+import fb from "../../assets/fb.png";
+import ig from "../../assets/ig.png";
+import ytb from "../../assets/ytb.png";
+import twitter from "../../assets/twitter.png";
+import lkdln from "../../assets/lkdln.png";
+import S from "./footer.module.scss";
+
+export default function Footer() {
+  return (
+    <footer>
+      <section className={S.footerVNW}>
+        <h3>4002-8922</h3>
+        <nav>
+          <a href="">
+            {" "}
+            <img src={fb} alt="" />
+          </a>
+          <a href="">
+            {" "}
+            <img src={twitter} alt="" />
+          </a>
+          <a href="">
+            {" "}
+            <img src={ytb} alt="" />
+          </a>
+          <a href="">
+            {" "}
+            <img src={lkdln} alt="" />
+          </a>
+          <a href="">
+            {" "}
+            <img src={ig} alt="" />
+          </a>
+        </nav>
+      </section>
+      <section className={S.footerDireitos}>
+        <p>Layout desenvolvido pela Vai Na Web para fins educativos - 2024 </p>
+      </section>
+    </footer>
+  );
 }
